8f3a5c72aa0f5bf6d4cf1a5804e1ee29636019ce
TeJaswiMiriyala <tejaswimiriyala9@gmail.com>
2020-02-24 08:35:48 -0500

    Demo for RPA using Python

---
+++ data1.py
@@ -0,0 +1,72 @@
+<html app='chrome.exe' title='Computer Books Online Shopping- Graphics | Networking books | Ga*' />
+<webctrl id='search' tag='INPUT' />
+
+<html app='chrome.exe' title='Computer Books Online Shopping- Graphics | Networking books | Ga*' />
+<webctrl id='search' tag='INPUT' />
+
+
+
+<wnd app='mstsc.exe' cls='TscShellContainerClass' title='172.17.10.43 - Remote Desktop Connection' />
+<wnd cls='IHWindowClass' title='Input Capture Window' />
+
+<wnd app='mstsc.exe' cls='TscShellContainerClass' title='172.17.10.43 - Remote Desktop Connection' />
+<wnd cls='IHWindowClass' title='Input Capture Window' />
+
+
+
+
+*List Of Machine Learning Libraries:
+
+----->*NumPy*:-   
+                 It is a library for the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical 
+                 functions to operate on these arrays.
+
+------->*SciPy*:-
+          It is a free and open-source Python library used for scientific computing and technical computing. SciPy contains modules for optimization, linear algebra, integration, 
+        interpolation, special functions, FFT, signal and image processing, ODE solvers and other tasks common in science and engineering.
+
+------->*Scikit-learn*:- It is probably the most useful library for machine learning in Python. It is on NumPy, SciPy and matplotlib, this library contains a lot of effiecient tools for 
+machine learning and statistical modeling including classification, regression, clustering and dimensionality reduction.
+
+-------->*Theano*:- It is a Python library and optimizing compiler for manipulating and evaluating mathematical expressions, especially matrix-valued ones. In Theano, computations are expressed 
+using a NumPy-esque syntax and compiled to run efficiently on either CPU or GPU architectures.
+
+-------->*TensorFlow*:- it is a free and open-source software library for dataflow and differentiable programming across a range of tasks. It is a symbolic math library, and is also used for 
+machine learning applications such as neural networks.
+
+--------->*Keras*:- It is an open-source neural-network library written in Python. It is capable of running on top of TensorFlow, Microsoft Cognitive Toolkit, R, Theano, or PlaidML. Designed to
+ enable fast experimentation with deep neural networks, it focuses on being user-friendly, modular, and extensible
+
+---------->*PyTorch*:- It is an open source machine learning library based on the Torch library, used for applications such as computer vision and natural language processing. It is primarily 
+developed by Facebook's AI Research lab. It is free and open-source software released under the Modified BSD license.
+
+--------->*Pandas*:- Pandas is a software library written for the Python programming language for data manipulation and analysis. In particular, it offers data structures and operations for
+ manipulating numerical tables and time series.
+
+-------->*Matplotlib*:- It is a plotting library for the Python programming language and its numerical mathematics extension NumPy. It provides an object-oriented API for embedding plots into
+ applications using general-purpose GUI toolkits like Tkinter, wxPython, Qt, or GTK+.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ defScript.py
@@ -0,0 +1,5 @@
+import os
+
+def add():
+        result=5+6
+        return result  
